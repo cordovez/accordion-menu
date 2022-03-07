20ba18dc3bf6428ccbcfbd33b29099c82b0b22bd
JC CorMan <cordovez@el-cordovez.com>
2022-03-07 13:24:53 +0100

    first push. Working on console log

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import data from "./data.json";
+// import { useState } from "react";
 import "./App.css";
-
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
-  );
+  // Find non-special category objects ...
+  let categories = data.filter((obj) => {
+    return !obj.parent_id && !obj.is_special;
+  });
+  // Find non-special subcategory objects
+  let subCategories = data.filter((obj) => {
+    return obj.parent_id && !obj.is_special;
+  });
+
+  // create an empty container for them
+  let orderedCategories = [];
+
+  // re-arrange them
+  categories.forEach((category) => {
+    orderedCategories.push(category);
+    if (!category.subCategories) {
+      category.subCategories = [];
+      subCategories.forEach((subCategory) => {
+        if (category._id === subCategory.parent_id) {
+          category.subCategories.push(subCategory);
+        }
+      });
+    }
+  });
+  console.log(categories);
+  return null;
 }
 
 export default App;
